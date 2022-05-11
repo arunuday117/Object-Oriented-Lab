220fb3eecdd5de1171065b8d4e4627b36c6db2d9
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2022-05-11 16:11:39 +0530

    Add files via upload

---
+++ CO3/co3_pg4.java
@@ -0,0 +1,24 @@
+class publisher {
+	String name,author;
+	
+}
+class book {
+	String b_name;
+	int isbn,price;
+	book(String name,String author){
+		
+	}
+}
+class literature extends book{
+	literature(String name,String author){
+		super(name,author);
+	}
+}
+class fiction extends literature{
+	fiction(String name,String author){
+		super(name,author);
+	}
+}
+public class co3_pg4 {
+
+}
